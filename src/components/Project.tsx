c066fc1d794781713564324e6fd5ea1133dd1298
newracket <newracket@users.noreply.github.com>
2023-04-20 23:13:27 -0700

    Added padding in project descriptions

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -4,29 +4,21 @@ export default function Project(projectData: ProjectType) {
   return (
     <div className="flex flex-col justify-between max-w-sm m-4 pb-4 border-2 border-stone-700 rounded-md overflow-hidden">
       <a href={projectData.link} target="_blank">
-        <img
-          src={`assets/projects/${projectData.image}`}
-          alt={projectData.image}
-        />
+        <img src={`assets/projects/${projectData.image}`} alt={projectData.image} />
       </a>
 
       <div className="mt-3">
         <a href={projectData.link} target="_blank" className="max-w-fit">
-          <h3 className="inline bg-slate-100 p-0.5 rounded-sm hover:bg-slate-200 font-semibold">
-            {projectData.title}
-          </h3>
+          <h3 className="inline bg-slate-100 p-0.5 rounded-sm hover:bg-slate-200 font-semibold">{projectData.title}</h3>
         </a>
       </div>
 
-      <p>{projectData.description}</p>
+      <p className="pl-2 pr-2">{projectData.description}</p>
 
       <div className="flex justify-center mt-auto">
         {projectData.skills.map((skill, i) => {
           return (
-            <p
-              className="inline border-2 border-stone-700 rounded-md p-1.5 m-1"
-              key={i}
-            >
+            <p className="inline border-2 border-stone-700 rounded-md p-1.5 m-1" key={i}>
               {skill}
             </p>
           );
